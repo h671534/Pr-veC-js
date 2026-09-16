95a46cd1e0c6080069c9fd81ac36815bd3f40e5d
h671534 <141551237+h671534@users.noreply.github.com>
2026-09-16 19:50:55 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,103 @@
+
+// console.log("Hei amelia");
+
+// let x = 77;
+// let v1 = 4;
+
+
+
+// x++;   // x = x + 1;
+
+// v1 += 4;
+
+// console.log("Svar er :" + x);
+// console.log("Add er :" + v1);
+
+
+
+
+
+
+// // const d = 4.4 / 2.0;
+// // console.log(d);
+
+// const v = 6;
+// b = v % 2
+// console.log(" Svar er " + b);
+
+
+
+
+// const t = 8 % 3;
+// console.log(" % er" + t);
+
+
+// const myStr = " I am a \"double quoted\" string inside \"double quotes\". ";
+// console.log(myStr);
+
+
+
+const myStr2 = ` <a href="http://www.example.com" target="_blank">Link</a> `;
+console.log(myStr2);
+
+
+// console.log("firstlin\t\n   \\secondlin\t\nthirdlin");  // \t er tab gjør space
+
+// console.log("FirstLine\n\t\\SecondLine\nThirdLine");
+
+// console.log("This is the start." + " " + "This is the end.");
+
+let myStr = "This is the first sentence.";    // += 
+
+myStr += " " + "This is the second sentence.";
+
+console.log(myStr);
+
+console.log(" denne kures er".length);  // teller space også
+
+
+
+let lastNameLength = 0;           // change lastNameLength skal være 8
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName;
+lastNameLength = 8;                     // hvor mange består av 
+
+lastNameLength = lastName.length;
+
+console.log("Svar er : " + lastNameLength);
+
+
+
+const lastName1 = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName1[lastName1.length - 4]; // fine siste i ordet -1 siste
+
+console.log("Siste er  :" + lastLetterOfLastName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
